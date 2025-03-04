b38784fbdc455bfefe31e8e3d4916b0b2d57db51
Trifall <JerrenT@Trifall.com>
2025-03-04 01:16:17 -0500

    refactor configuration to use static environment variables and update adapter for production

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,16 +1,18 @@
-import { env } from "$env/dynamic/private";
-import { env as publicEnv } from "$env/dynamic/public";
+import { BETTER_AUTH_SECRET, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "$env/static/private";
+import { PUBLIC_BASE_URL } from "$env/static/public";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import { db } from "./db";
 
+if (!PUBLIC_BASE_URL) throw new Error("PUBLIC_BASE_URL is not set");
+
 export const auth = betterAuth({
-	baseURL: publicEnv.PUBLIC_BASE_URL!,
+	baseURL: PUBLIC_BASE_URL!,
 	database: drizzleAdapter(db, {
 		provider: "pg",
 	}),
-
+	secret: BETTER_AUTH_SECRET,
 	// https://www.better-auth.com/docs/concepts/session-management#session-caching
 	session: {
 		cookieCache: {
@@ -22,8 +24,8 @@ export const auth = betterAuth({
 	// https://www.better-auth.com/docs/concepts/oauth
 	socialProviders: {
 		github: {
-			clientId: env.GITHUB_CLIENT_ID!,
-			clientSecret: env.GITHUB_CLIENT_SECRET!,
+			clientId: GITHUB_CLIENT_ID!,
+			clientSecret: GITHUB_CLIENT_SECRET!,
 		},
 		// google: {
 		// 	clientId: env.GOOGLE_CLIENT_ID!,
--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,11 +1,11 @@
-import { env } from "$env/dynamic/private";
+import { DATABASE_URL } from "$env/static/private";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 import * as schema from "./schema";
 
-if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
+if (!DATABASE_URL) throw new Error("DATABASE_URL is not set priv");
 
-const driver = postgres(env.DATABASE_URL);
+const driver = postgres(DATABASE_URL);
 
 export const db = drizzle({ client: driver, schema });
