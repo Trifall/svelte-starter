2b65d8309c6d9d5fab604aa2488bfcd84bcb93cb
Trifall <JerrenT@Trifall.com>
2025-03-03 23:23:19 -0500

    refactor auth module to use dynamic public environment variables and remove unused index file

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,12 +1,12 @@
 import { env } from "$env/dynamic/private";
-import { PUBLIC_BASE_URL } from "$env/static/public";
+import { env as publicEnv } from "$env/dynamic/public";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import { db } from "./db";
 
 export const auth = betterAuth({
-	baseURL: PUBLIC_BASE_URL,
+	baseURL: publicEnv.PUBLIC_BASE_URL!,
 	database: drizzleAdapter(db, {
 		provider: "pg",
 	}),
@@ -25,14 +25,14 @@ export const auth = betterAuth({
 			clientId: env.GITHUB_CLIENT_ID!,
 			clientSecret: env.GITHUB_CLIENT_SECRET!,
 		},
-		google: {
-			clientId: env.GOOGLE_CLIENT_ID!,
-			clientSecret: env.GOOGLE_CLIENT_SECRET!,
-		},
-		discord: {
-			clientId: env.DISCORD_CLIENT_ID!,
-			clientSecret: env.DISCORD_CLIENT_SECRET!,
-		},
+		// google: {
+		// 	clientId: env.GOOGLE_CLIENT_ID!,
+		// 	clientSecret: env.GOOGLE_CLIENT_SECRET!,
+		// },
+		// discord: {
+		// 	clientId: env.DISCORD_CLIENT_ID!,
+		// 	clientSecret: env.DISCORD_CLIENT_SECRET!,
+		// },
 	},
 
 	// https://www.better-auth.com/docs/authentication/email-password
